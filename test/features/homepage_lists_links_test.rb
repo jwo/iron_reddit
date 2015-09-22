5b3cc9578b14daed6f40f39ad0092887e5a88997
Jesse Wolgamott <jesse@comalproductions.com>
2015-09-22 10:24:47 -0500

    Fix homepage; load all links on page

--- test/features/homepage_lists_links_test.rb
+++ test/features/homepage_lists_links_test.rb
@@ -1,9 +1,12 @@
 require "test_helper"
 
 class HomepageListsLinksTest < Capybara::Rails::TestCase
-  test "sanity" do
+  test "Homepage lists links in db" do
+
+    link = Link.create title: "The 31 Most Disturbing Emojis Of This Year"
+
     visit root_path
-    assert_content page, "Hello World"
-    refute_content page, "Goobye All!"
+
+    assert_content page, link.title
   end
 end
